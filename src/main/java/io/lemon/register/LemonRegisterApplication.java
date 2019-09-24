a2c99522b82194257432e1e7a203cd4bd2f21c8f
徐佳庆 <jiaqing.xu@hand-china.com>
2019-09-24 21:18:34 +0800

    [FIX] 初始化项目

---
+++ src/main/java/io/lemon/register/LemonRegisterApplication.java
@@ -0,0 +1,13 @@
+package io.lemon.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemonRegisterApplication.class, args);
+    }
+
+}
